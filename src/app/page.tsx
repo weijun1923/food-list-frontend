9330495511157aedb5aeaa06d1841437b09ee289
哖芯瑜 <nianxinyu11@gmail.com>
2025-05-09 12:57:41 +0800

    feat(login ui): login ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
 import Image from "next/image";
-import{Button} from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+
+// 連結登入頁面
+import Link from "next/link";
+
 
 export default function Home() {
   return (
-  <Button>Button</Button> 
+  <Button>
+    <Link href="/login">Login</Link>
+    </Button> 
   );
 }
+
+
